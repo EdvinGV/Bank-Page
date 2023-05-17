0564f2ab639ca48ff42544b044d3bd3a3bbd7ae9
Edvinas <edvingvozdovic@gmail.com>
2023-05-17 13:51:36 +0300

    Bank Page

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.scss';
+import { Bankas } from './Eg-Bankas';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div >
+      <header className="App-header">
+  <Bankas/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
